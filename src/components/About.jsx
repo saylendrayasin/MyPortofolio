0df7d02aa8f58fd3753531485ca42f80a34e93ea
Saylendra Yasin <saylendrayasin@gmail.com>
2022-11-26 18:21:46 +0800

    Fix styling about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,23 +5,25 @@ function About() {
     <section id="about" className="pt-36 pb-32">
       <div className="container">
         <div className="flex flex-wrap">
-          <div className="w-full px-4 mb-10">
+          <div className="w-full px-4 mb-10 lg:w-1/2">
             <h3 className="font-bold uppercase text-primary text-lg mb-3">
               About Me
             </h3>
-            <h2 className="font-bold text-dark text-3xl mb-5 max-w-md">
+            <h2 className="font-bold text-dark text-3xl mb-5 max-w-md lg:text-4xl">
               Come on with me
             </h2>
-            <p className="font-medium text-base text-second max-w-xl">
+            <p className="font-medium text-base text-second max-w-xl lg:text-lg">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
           </div>
-          <div className="w-full px-4">
-            <h3 className="font-semibold text-dark text-2xl mb-4">Follow Me</h3>
-            <p className="font-medium text-base text-second mb-6">
+          <div className="w-full px-4 lg:w-1/2">
+            <h3 className="font-semibold text-dark text-2xl mb-4 lg:text-3xl lg:pt-10">
+              Follow Me
+            </h3>
+            <p className="font-medium text-base text-second mb-6 lg:text-lg">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since
